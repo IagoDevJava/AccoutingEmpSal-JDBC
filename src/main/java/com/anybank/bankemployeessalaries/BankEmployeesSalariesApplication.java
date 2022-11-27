56f1da15340ff07a00c5acaeb67b237baf09fc72
Egorov Ivan <iago.devs@yandex.ru>
2022-11-27 14:25:03 +0600

    first commit first commit

---
+++ src/main/java/com/anybank/bankemployeessalaries/BankEmployeesSalariesApplication.java
@@ -0,0 +1,13 @@
+package com.anybank.bankemployeessalaries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankEmployeesSalariesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankEmployeesSalariesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/anybank/bankemployeessalaries/BankEmployeesSalariesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anybank.bankemployeessalaries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankEmployeesSalariesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
